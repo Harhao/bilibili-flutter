a723bc75d38acabc778a437c1e5b1c3dfaf5348e
Harhao <huahaoworkspace@163.com>
2020-07-25 11:33:34 +0800

    feat: 添加自定义icon图标和底部导航栏

---
+++ lib/config/customIcon.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+
+class CustomIcon {
+  static const IconData settting =
+      const IconData(0xe61a, fontFamily: 'custom', matchTextDirection: true);
+  static const IconData shopCart =
+      const IconData(0xe61c, fontFamily: 'custom', matchTextDirection: true);
+  static const IconData home =
+      const IconData(0xe617, fontFamily: 'custom', matchTextDirection: true);
+  static const IconData channel =
+      const IconData(0xe756, fontFamily: 'custom', matchTextDirection: true);
+  static const IconData dynamicState =
+      const IconData(0xe62d, fontFamily: 'custom', matchTextDirection: true);
+  static const IconData email =
+      const IconData(0xe670, fontFamily: 'custom', matchTextDirection: true);
+  static const IconData download =
+      const IconData(0xe705, fontFamily: 'custom', matchTextDirection: true);
+  static const IconData memberShop =
+      const IconData(0xe601, fontFamily: 'custom', matchTextDirection: true);
+}
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../config/customIcon.dart';
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
 
-class Home extends StatelessWidget {
+class _HomeState extends State<Home> {
+  int _selectedIndex = 0;
   @override
   Widget build(BuildContext context) {
     final List<Tab> Tabs = <Tab>[
@@ -9,12 +15,14 @@ class Home extends StatelessWidget {
       Tab(text: '推荐'),
       Tab(text: '追番'),
       Tab(text: '影视'),
-      Tab(text: '说唱'),
+      Tab(text: '说唱区'),
+      Tab(text: '动漫'),
     ];
     return DefaultTabController(
         length: Tabs.length,
         child: Scaffold(
           appBar: AppBar(
+              centerTitle: true,
               elevation: 1.0,
               leading: Padding(
                   padding: EdgeInsets.all(8.0),
@@ -52,13 +60,13 @@ class Home extends StatelessWidget {
               actions: <Widget>[
                 IconButton(
                     icon: Icon(
-                      Icons.email,
+                      CustomIcon.email,
                       color: Colors.white,
                     ),
                     onPressed: null),
                 IconButton(
                     icon: Icon(
-                      Icons.grain,
+                     CustomIcon.download,
                       color: Colors.white,
                     ),
                     onPressed: null),
@@ -68,8 +76,10 @@ class Home extends StatelessWidget {
                   child: Material(
                     color: Colors.white,
                     child: TabBar(
+                      isScrollable: true,
+                      labelStyle: TextStyle(fontSize: 20.0),
                       labelColor: Theme.of(context).primaryColor,
-                      unselectedLabelColor: Color(0xff000000),
+                      unselectedLabelColor: Color(0xff8a8a8a),
                       indicatorColor: Theme.of(context).primaryColor,
                       indicatorSize: TabBarIndicatorSize.label,
                       indicatorWeight: 3.0,
@@ -83,6 +93,27 @@ class Home extends StatelessWidget {
           drawer: Drawer(
             child: ListView(children: <Widget>[]),
           ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            fixedColor: Theme.of(context).primaryColor,
+            unselectedItemColor: Color(0xff8b8b8b),
+            unselectedLabelStyle: TextStyle(color:Color(0xff8b8b8b)),
+            unselectedFontSize: 14.0,
+            currentIndex: _selectedIndex,
+            onTap: selectedBottomBar,
+            items: <BottomNavigationBarItem>[
+              BottomNavigationBarItem(icon: Icon(CustomIcon.home),title: Text('首页')),
+              BottomNavigationBarItem(icon: Icon(CustomIcon.channel),title: Text('频道')),
+              BottomNavigationBarItem(icon: Icon(CustomIcon.dynamicState),title: Text('动态')),
+              BottomNavigationBarItem(icon: Icon(CustomIcon.memberShop),title: Text('会员购')),
+            ]
+          ),
         ));
+  }
+
+  void selectedBottomBar(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
   }
 }
